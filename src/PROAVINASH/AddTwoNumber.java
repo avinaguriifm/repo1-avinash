a6b4e79cf599f689628c0a0546fa4c29357ba17e
avinaguriifm <115398292+avinaguriifm@users.noreply.github.com>
2022-10-11 21:58:57 +0530

    Update AddTwoNumber.java

--- src/PROAVINASH/AddTwoNumber.java
+++ src/PROAVINASH/AddTwoNumber.java
@@ -5,7 +5,8 @@ public class AddTwoNumber {
 	public static void main(String[] args) {
 		int a=3;
 		int b=4;
-		System.out.println("sum of two number is: "+(a+b));
+		int c=5;
+		System.out.println("sum of two number is: "+(a+b+c));
 
 	}
 
