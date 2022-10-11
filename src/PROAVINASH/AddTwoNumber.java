0cbcc9c49984d96c4ed5188eae63f0e8bb31b340
avinash kumar <av.singh19@gmail.com>
2022-10-11 20:28:19 +0530

    commit the project

---
+++ src/PROAVINASH/AddTwoNumber.java
@@ -0,0 +1,12 @@
+package PROAVINASH;
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		System.out.println("sum of two number is: "+(a+b));
+
+	}
+
+}
